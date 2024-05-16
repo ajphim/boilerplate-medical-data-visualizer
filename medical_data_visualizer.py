2b8468bec37eb54f1cece328d8c8566f5e6e5e70
Andrew Phimvongsa <ajphim@hotmail.com>
2024-05-16 23:36:27 +0000

    catplot done

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -17,18 +17,28 @@ df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
 
 # 4: Draw Categorical plot
 def draw_cat_plot():
-    # 5
-    df_cat = None
-
-    # 6
-    df_cat = None
+    # 5: Create DataFrame for catplot using pd.melt()
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
     
-    # 7
+    # 6: Group and reformat data
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_index=False).size()
 
-    # 8
-    fig = None
+    # 7: Rename size column to total for cat plot to work
+    df_cat.rename(columns={'size': 'total'}, inplace=True)
 
-    # 9
+    # 8: Create a catplot
+    fig = sns.catplot(
+        x='variable',
+        y='total',
+        hue='value',
+        col='cardio',
+        data=df_cat,
+        kind='bar',
+        height=5,
+        aspect=1
+    ).fig
+
+    # 9: Get figure for the output and store it in fig variable
     fig.savefig('catplot.png')
     return fig
 
@@ -45,10 +55,10 @@ def draw_heat_map():
     mask = None
 
     # 14
-    fig, ax = None
+    #fig, ax = None
 
     # 15
-    
+
     # 16
-    fig.savefig('heatmap.png')
-    return fig
+    #fig.savefig('heatmap.png')
+    #return fig
